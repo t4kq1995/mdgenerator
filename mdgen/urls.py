4a95c69d1fb2c13fd79811a404c39a55f0523516
t4kq1995 <vlad13zp@mail.ru>
2017-07-22 14:56:46 +0300

    Make views for data

--- mdgen/urls.py
+++ mdgen/urls.py
@@ -8,4 +8,10 @@ from mdgen import views
 urlpatterns = [
     # Auth
     url(r'^auth_user/$', views.login),
+    # Get country
+    url(r'^get_country/$', views.get_country_full),
+    # User data
+    url(r'^get_user_data/$', views.get_user_data),
+    # Country data
+    url(r'^get_country_data/$', views.get_country_data),
 ]
--- mdgen/views.py
+++ mdgen/views.py
@@ -8,10 +8,13 @@ from rest_framework import permissions
 from django.http import JsonResponse
 
 # Models
-from mdgen.models import Member
+from mdgen.models import Member, Country, UserData, CountryData
 
 # Exceptions
 from django.core.exceptions import ObjectDoesNotExist
+
+# Random
+import random
 
 
 @api_view(['POST'])
@@ -29,3 +32,64 @@ def login(request):
             except ObjectDoesNotExist:
                 return JsonResponse({'status': False})
         return JsonResponse({'status': False})
+
+
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+def get_country_full(request):
+    """ Get full country name """
+    if request.method == 'POST':
+        country = request.data.get('country', '')
+        if country != '':
+	        try:
+		        country_object = Country.objects.get(country=country)
+		        return JsonResponse({'status': True, 'country_full': country_object.country_full})
+	        except ObjectDoesNotExist:
+		        return JsonResponse({'status': False})
+        return JsonResponse({'status': False})
+    
+
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+def get_user_data(request):
+    """ Get full country name """
+    if request.method == 'POST':
+	    amount = request.data.get('amount', '')
+	    if amount != '':
+	        try:
+		        answer = []
+		        user_data = UserData.objects.all()
+		        for element in user_data:
+			        element_data = element.data.split(',')
+			        random.shuffle(element_data)
+			        answer.append(
+				        {'type': element.type,
+				         'data': [s.replace("'", "").strip() for s in element_data[:int(amount)]]
+				         })
+		        return JsonResponse({'status': True, 'user_data': answer})
+	        except ObjectDoesNotExist:
+		        return JsonResponse({'status': False})
+	    return JsonResponse({'status': False})
+
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+def get_country_data(request):
+    """ Get full country name """
+    if request.method == 'POST':
+	    amount = request.data.get('amount', '')
+	    country = request.data.get('country', '')
+	    if amount != '' and country != '':
+	        try:
+		        answer = []
+		        country_data = CountryData.objects.get(country=country)
+		        data_country = country_data.data.split('\n')
+		        random.shuffle(data_country)
+		        return JsonResponse(
+			        {
+				        'status': True,
+			            'country_data': [s.replace("\r", "").strip() for s in data_country[:int(amount)]]
+			        }
+		        )
+	        except ObjectDoesNotExist:
+		        return JsonResponse({'status': False})
+	    return JsonResponse({'status': False})
